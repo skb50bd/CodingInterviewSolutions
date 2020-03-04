e2d5e378015a324902e3afa21a3310a92b49a55c
Shakib Haris <skb50bd@gmail.com>
2020-03-04 08:42:05 +0600

    Added More Tests for Spiral Matrix

--- DOTNET_CSharp/Tests/SpiralMatrixTests.cs
+++ DOTNET_CSharp/Tests/SpiralMatrixTests.cs
@@ -10,11 +10,13 @@ namespace Tests {
                 new[] { 01, 02, 03, 04 },
                 new[] { 05, 06, 07, 08 },
                 new[] { 09, 10, 11, 12 },
-                new[] { 13, 14, 15, 16 } 
-            };
+                new[] { 13, 14, 15, 16 } };
+
             var expected = new[] {
-                1, 2, 3, 4, 8, 12, 16, 15, 14, 13, 9, 5, 6, 7, 11, 10
-            };
+                01, 02, 03, 04, 
+                08, 12, 16, 15, 
+                14, 13, 09, 05, 
+                06, 07, 11, 10 };
 
             // Act
             var actual = input.TraverseSpiral();
@@ -29,11 +31,44 @@ namespace Tests {
             var input = new int[][] {
                 new [] { 1, 2, 3 },
                 new [] { 4, 5, 6 },
-                new [] { 7, 8, 9 }
-            };
-            var expected = new[] {
-                1, 2, 3, 6, 9, 8, 7, 4, 5
-            };
+                new [] { 7, 8, 9 } };
+
+            var expected = new[] { 
+                1, 2, 3, 
+                6, 9, 8, 
+                7, 4, 5 };
+
+            // Act
+            var actual = input.TraverseSpiral();
+            
+            // Assert
+            Assert.Equal(expected, actual);
+        }
+    
+        [Fact]
+        public void SingleElement() {
+            // Arrange
+            var input = new int[][] { new [] { 1 } };
+
+            var expected = new[] { 1 };
+
+            // Act
+            var actual = input.TraverseSpiral();
+            
+            // Assert
+            Assert.Equal(expected, actual);
+        }
+
+        [Fact]
+        public void VerticalMatrix() {
+            // Arrange
+            var input = new int[][] { 
+                new [] { 1, 2 }, 
+                new [] { 3, 4 }, 
+                new [] { 5, 6 }, 
+                new [] { 7, 8 } };
+
+            var expected = new[] { 1, 2, 4, 6, 8, 7, 5, 3 };
 
             // Act
             var actual = input.TraverseSpiral();
