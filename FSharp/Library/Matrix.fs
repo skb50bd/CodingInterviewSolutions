91ba237dd71e5134de3c56a027ec2b0e4a2b54a4
Shakib Haris <skb50bd@gmail.com>
2020-03-22 09:42:13 +0600

    Refactored

--- FSharp/Library/Matrix.fs
+++ FSharp/Library/Matrix.fs
@@ -1,7 +1,7 @@
 namespace Library
 
 module Matrix =
-    type private Direction =
+    type Direction =
         | Right = 0
         | Down = 1
         | Left = 2
@@ -9,8 +9,9 @@ module Matrix =
 
     let TraverseSpiral matrix =
         match Array2D.length1 matrix with
-        | 0 -> Array.empty
-
+        | 0 ->
+            Array.empty
+            
         | height ->
             let width = Array2D.length2 matrix
 
@@ -22,30 +23,27 @@ module Matrix =
 
                 else
                     let rest() = traverse (rotate dir) l r t b
-
                     let rest'() = traverse (rotate dir) (l + 1) (r - 1) (t + 1) (b - 1)
 
-                    match enum<Direction> dir with
-                    | Direction.Right ->
-                        let current =
+                    let d = enum<Direction> dir
+
+                    let current =
+                        match d with
+                        | Direction.Right ->
                             [| for i in [ l .. r ] -> matrix.[t, i] |]
-                        [| yield! current; yield! rest() |]
-  
-                    | Direction.Down ->
-                        let current =
+
+                        | Direction.Down ->
                             [| for i in [ (t + 1) .. b ] -> matrix.[i, r] |]
-                        [| yield! current; yield! rest() |]
 
-                    | Direction.Left ->
-                        let current =
+                        | Direction.Left ->
                             [| for i in [ (r - 1) .. (-1) .. l ] -> matrix.[b, i] |]
-                        [| yield! current; yield! rest() |]
 
-                    | Direction.Up ->
-                        let current =
+                        | Direction.Up ->
                             [| for i in [ (b - 1) .. (-1) .. (t + 1) ] -> matrix.[i, l] |]
-                        [| yield! current; yield! rest'() |]
 
-                    | _ -> Array.empty
+                        | _ -> Array.empty
+
+                    [| yield! current
+                       yield! if d = Direction.Up then rest'() else rest() |]
 
             traverse 0 0 (width - 1) 0 (height - 1)
